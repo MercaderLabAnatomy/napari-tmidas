12c50b911fe5f500f9061c7d5a6dffd583a9e470
macromeer <marco.meer@pm.me>
2025-04-03 15:50:47 +0200

    added roi colocalization analyzer

--- src/napari_tmidas/__init__.py
+++ src/napari_tmidas/__init__.py
@@ -6,6 +6,7 @@ except ImportError:
 
 from ._label_inspection import label_inspector_widget
 from ._reader import napari_get_reader
+from ._roi_colocalization import roi_colocalization_analyzer
 from ._sample_data import make_sample_data
 from ._writer import write_multiple, write_single_image
 
@@ -17,4 +18,5 @@ __all__ = (
     "file_selector",
     "label_inspector_widget",
     "batch_crop_anything_widget",
+    "roi_colocalization_analyzer",
 )
